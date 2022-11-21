cbbe373d2b32c6d6ca3c6bce0eac93ee3e602da7
Ben Greig <aquillo@bigpond.net.au>
2022-11-21 15:08:42 +1100

    added a booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :meals
+  validates :date, :size, :price, presence: true
+  enum :status, { pending: 0, confirmed: 1, rejected: 2}
 end
