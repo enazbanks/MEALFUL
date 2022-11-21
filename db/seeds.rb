0e4651079375de1100cc743ea6b7958192765770
Ben Greig <aquillo@bigpond.net.au>
2022-11-21 16:52:33 +1100

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    age: Faker::Number.within(range: 18..99),
+    email: Faker::Internet.email
+  )
+  user.save!(:validate => false)
+end
+
+50.times do
+  meal = Meal.new(
+    name: Faker::Hipster.sentence,
+    description: Faker::Food.description,
+    size: Faker::Number.within(range: 1..25),
+    price: Faker::Number.decimal(l_digits: 2, r_digits: 2),
+    category: Faker::Food.ingredient,
+    location: Faker::Address.city
+  )
+  meal.user = User.all.sample
+  if meal.save
+    puts "#{meal.name} saved"
+  else
+    puts "something went wrong"
+  end
+end
