dd67c23589be8d99eb288837fbe513aa96b47cf2
Bradley Tucker <bradley_tucker@hotmail.co.uk>
2022-11-23 14:58:50 +1100

    add rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,12 +2,8 @@ class RatingsController < ApplicationController
   def new
     # TODO
     @booking = Booking.find(params[:booking_id])
-    if @booking.confirmed? && Date.today >= @booking.date
       @rating = Rating.new
       authorize @rating
-    else
-      redirect_to meals_path(@meal), status: :see_other, notice: "Unable to create a review"
-    end
   end
 
   def create
